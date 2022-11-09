dea59cc293e9391a468ddf589def9ffe6cf2d454
dmsc0 <94014777+dmsc0@users.noreply.github.com>
2022-11-09 10:41:54 +0200

    Update pb7.c

--- assignment_1/pb7.c
+++ assignment_1/pb7.c
@@ -4,7 +4,8 @@
 int main()
 {
 	double diag1, diag2;
-	printf("Please type in the lenght of the diagonals of the diamond shape (separated by spaces): ");
+	printf("Please type in the lenght of the diagonals of \
+	the diamond shape (separated by spaces): ");
 	scanf("%lf %lf", &diag1, &diag2);
 
 	printf("\n\nThe area of the diamond shape is %.2f . \n", (diag1*diag2)/2);
