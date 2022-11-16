adc83716ed5cceb62dcc8054ef9e43f68d0839dd
dmsc0 <94014777+dmsc0@users.noreply.github.com>
2022-11-16 10:32:31 +0200

    Create pb3.c

---
+++ assignment_2/pb3.c
@@ -0,0 +1,83 @@
+/*
+
+Write a program to read an integer with max 3 digits and print the value in words(111 will be printed as one hundred and eleven)
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int digits[3];
+	char digit[10][25], tens[10][25], ten_to_twenty[10][25];
+
+	strcpy(digit[1], "one");
+	strcpy(digit[2], "two");
+	strcpy(digit[3], "three");
+	strcpy(digit[4], "four");
+	strcpy(digit[5], "five");
+	strcpy(digit[6], "six");
+	strcpy(digit[7], "seven");
+	strcpy(digit[8], "eight");
+	strcpy(digit[9], "nine");
+
+	strcpy(tens[2], "twenty");
+	strcpy(tens[3], "thirty");
+	strcpy(tens[4], "fourty");
+	strcpy(tens[5], "fifty");
+	strcpy(tens[6], "sixty");
+	strcpy(tens[7], "seventy");
+	strcpy(tens[8], "eighty");
+	strcpy(tens[9], "ninety");
+
+	strcpy(ten_to_twenty[0], "ten");
+	strcpy(ten_to_twenty[1], "eleven");
+	strcpy(ten_to_twenty[2], "twelve");
+	strcpy(ten_to_twenty[3], "thirteen");
+	strcpy(ten_to_twenty[4], "fourteen");
+	strcpy(ten_to_twenty[5], "fifteen");
+	strcpy(ten_to_twenty[6], "sixteen");
+	strcpy(ten_to_twenty[7], "seventeen");
+	strcpy(ten_to_twenty[8], "eighteen");
+	strcpy(ten_to_twenty[9], "nineteen");
+	
+	for(int i=2; i>=0; i--)
+		{
+				char temp;
+				scanf("%c", &temp);
+				
+				if(temp >= '0' && temp <='9')
+					digits[i]=temp-'0';
+				else 
+					{
+							printf("Wrong input! You need to type a 3 digits number ");
+							return 0;
+					}
+		}
+	
+	for(int i=2; i>=0; i--)
+		{
+			switch(i){
+				case 0: // digit
+							if(digits[1] != 1 && digits[0] != 0) printf("%s ", digit[digits[i]]);
+							break;
+
+				case 1: // tens
+							if(digits[1] == 0) continue;
+							if(digits[1] != 1) printf("%s ", tens[digits[i]]);
+							else printf("%s ", ten_to_twenty[digits[i-1]]);
+							break;
+
+				case 2: // hundreds 
+							printf("%s hundred ", digit[digits[i]]);
+							if(digits[0] || digits[1]) printf("and ");
+
+							break;
+				
+			}
+		}
+		
+	
+	return 0;
+}
